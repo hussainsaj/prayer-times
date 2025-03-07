aca109ca63834b2196152410cc6e63d18151db1d
hussainsaj <hussain-sajid@outlook.com>
2025-03-07 19:43:16 +0000

    updated api calculations

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 import time
+import datetime
 from datetime import datetime
+from datetime import timedelta
 import schedule
 import pygame
 import math
@@ -12,7 +14,7 @@ LOCATION = {
     'city': 'Stoke-on-Trent',
     'country': 'United Kingdom'
 }
-API_URL = f"https://api.aladhan.com/v1/timingsByCity?city={LOCATION['city']}&country={LOCATION['country']}"
+API_URL = f"https://api.aladhan.com/v1/timingsByCity?city={LOCATION['city']}&country={LOCATION['country']}&latitudeAdjustmentMethod=2"
 
 def wait_for_network():
     # Function to check network connectivity
@@ -31,12 +33,17 @@ def get_prayer_times():
         response = requests.get(API_URL)
         response.raise_for_status()  # Raise an exception for HTTP errors
         data = response.json()
+        
+        #for testing purposes
+        #now_plus_two = datetime.now() + timedelta(minutes=2)
+        
         return {
             'Fajr': data['data']['timings']['Fajr'],
             'Dhuhr': data['data']['timings']['Dhuhr'],
             'Asr': data['data']['timings']['Asr'],
             'Maghrib': data['data']['timings']['Maghrib'],
             'Isha': data['data']['timings']['Isha']
+            #,'Test': now_plus_two.strftime('%H:%M')
         }
     except requests.RequestException as e:
         print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Error fetching prayer times: {e}")
@@ -50,20 +57,22 @@ def play_sound(label):
         source = None
 
         if label == 'Fajr':
-            source = 'audio/f.mp3'
+            source = 'audio/f.wav'
         else:
-            source = 'audio/a.mp3'
+            source = 'audio/a.wav'
 
         if not os.path.exists(source):
             raise FileNotFoundError(f"Audio file '{source}' does not exist.")
 
+        pygame.mixer.pre_init(frequency=44100, size=-16, channels=1, buffer=4096, devicename=None)
         pygame.init()
+        
         athan = pygame.mixer.Sound(source)
         athan_length = math.ceil(athan.get_length() * 1000) + 1000
 
         athan.play()
         pygame.time.wait(athan_length) #wait till the athan is finished before closing the programme
-
+        
         print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Finished playing athan for {label}")
 
     except FileNotFoundError as e:
@@ -90,6 +99,8 @@ def main():
     timings_retrieved = False
     timings_scheduled = False
     last_heartbeat = time.time()
+    
+    wait_for_network()
 
     while True:
         time.sleep(1)
